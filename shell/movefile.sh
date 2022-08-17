801e897a7614f21a27892d2587d309e5709ef07d
Kishou <xijie931025@gmail.com>
2022-08-18 00:14:02 +0900

    Update movefile.sh

--- shell/movefile.sh
+++ shell/movefile.sh
@@ -17,7 +17,7 @@ echo -n QUESTION?
 read q
 mkdir -p _result/_$dir/$q
 mv -i $sendfile _result/_$dir/$q
-git add _result/_$dir/$q/$sendfile
+git add -f _result/_$dir/$q/$sendfile
 git commit -m "$dir $q"
 cp -i _template/$sendfile ./$sendfile
 
