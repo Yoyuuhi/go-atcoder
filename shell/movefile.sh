1c01b704e05dedaf0da04804cde6c31158bb22ed
Kishou <xijie931025@gmail.com>
2022-09-11 01:42:31 +0900

    Update movefile.sh

--- shell/movefile.sh
+++ shell/movefile.sh
@@ -15,8 +15,8 @@ dir=$(cat ./contest)
 echo -e CONTEST: $dir
 echo -n QUESTION?
 read q
-mkdir -p _result/_$dir/$q
-mv -i $sendfile _result/_$dir/$q
+mkdir -p _result/_$dir/${q// /_}
+mv -i $sendfile _result/_$dir/${q// /_}
 git add -f _result/_$dir/$q/$sendfile
 git commit -m "$dir $q"
 cp -i _template/$sendfile ./$sendfile
